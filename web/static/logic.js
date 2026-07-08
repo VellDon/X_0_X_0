64a46d272d6f45ef8495080eff00b90ae5b1b8d6
annettta <annettta>
2026-07-08 17:08:55 +0300

    1vs1

--- web/static/logic.js
+++ web/static/logic.js
@@ -1,4 +1,6 @@
 const myBox = document.getElementById('myBox');
+const MyForm = document.getElementById('MyForm');
+
 let btns = Array.from(myBox.querySelectorAll('button'));
 console.log("Какая то длина = " + serverMatrix.length);
 
@@ -21,6 +23,37 @@ myBox.addEventListener('click', function (event) {
 
 });
 
+MyForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let login = document.getElementById('login').value;
+    let password = document.getElementById('password').value;
+    Register(login, password);
+});
+async function Register(name, pass) {
+    const data = {
+        name: name,
+        password: pass
+    };
+    /*
+    const result = await fetch(`/registration`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    const res = await result.json();
+    if (res.status === "ok") {
+        MyForm.classList.add('MyFormOff');
+        myBox.classList.add('loyout');
+    }
+    */
+    MyForm.classList.add('MyFormOff');
+    myBox.classList.remove('loyoutOff')
+    myBox.classList.add('loyout');
+    alert(data);
+}
+
 async function Move(index) {
     const data = {
         number: index
@@ -38,13 +71,13 @@ async function Move(index) {
         btns[index].classList.add('StateX');
         let otvet = Number(res.move);
         btns[otvet].classList.add('State0');
-        if (res.win === "player"){
+        if (res.win === "player") {
             alert("Ты выйграл");
             location.reload();
-        } else if (res.win === "bot"){
+        } else if (res.win === "bot") {
             alert("Ты проиграл");
             location.reload();
-        } else if (res.win === "draw"){
+        } else if (res.win === "draw") {
             alert("Ничья");
             location.reload();
         }
